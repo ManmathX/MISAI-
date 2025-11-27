fe043d3aee1042cf6b3d31837171b687dd045c1a
ManmathX <manmath2006n@gmail.com>
2025-11-27 11:40:21 +0530

    add into video
    bhai intro video ka pr merged kar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,43 +1,55 @@
 import './App.css'
-import { useEffect } from 'react'
+import { useState } from 'react'
 import Home from './Pages/Home/Home'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import TestAI from './Pages/TestAI/TestAI'
 import TestImage from './Pages/TestImage/TestImage'
 import TestVideo from './Pages/TestVideo/TestVideo'
 import Misbot from './Pages/MisBot/MisBot'
+import IntroScreen from './Pages/IntroScreen/IntroScreen'
 
 
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
 
-const router = createBrowserRouter([
-{
-  path: '/',
-  element: <> <Home/> </>
-},
-{
-  path: '/testai',
-  element: <> <TestAI/> </>
-},
-{
-  path: '/testimage',
-  element: <> <TestImage/> </>
-},
-{
-  path: '/testvideo',
-  element: <> <TestVideo/> </>
-},
-{
-  path: '/misbot',
-  element: <> <Misbot/> </>
-},
-])
-  return(
+  const handleIntroComplete = () => {
+    setShowIntro(false);
+  };
 
-  <div className="app-container">
-<RouterProvider router={router} />
-  </div>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <> <Home /> </>
+    },
+    {
+      path: '/testai',
+      element: <> <TestAI /> </>
+    },
+    {
+      path: '/testimage',
+      element: <> <TestImage /> </>
+    },
+    {
+      path: '/testvideo',
+      element: <> <TestVideo /> </>
+    },
+    {
+      path: '/misbot',
+      element: <> <Misbot /> </>
+    },
+  ])
+
+  // Show intro screen first, then main app
+  if (showIntro) {
+    return <IntroScreen onComplete={handleIntroComplete} />;
+  }
+
+  return (
+
+    <div className="app-container">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
